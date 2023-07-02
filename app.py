242c9921ab18a975e2b08352a61f122ead911bbb
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2023-07-02 23:58:31 +0530

    refactored code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import json
 import requests
 from flask import Flask, request, jsonify
-from config import chat_collection, pb, db, chatv2_collection
+from config import chat_collection, pb, db
 
 from functools import wraps
 from firebase_admin import auth
@@ -71,21 +71,24 @@ def userInfo():
         return jsonify({'userInfo': request.user['email']}), 200
     except Exception as e:
         return f'{e}', 400
-    
-@app.route('/get', methods=['GET'])
+
+
+@app.route('/fetchMessagesByChatId', methods=['POST'])
+@check_token
 def getData():
+    uid = request.user['uid']
+    chat_id = request.json['chat_id']
+    type = request.json['type']
     try:
-        if chatv2_collection:
-            print('hello')
-            # here document is UID 
-            chats = db.collection('chat_v2').document('wOtArbf5bTZlvaBis1El').collection('messages').document('chat1').get()
-            print('chats----', chats.to_dict())
-            # for chat in chats:
-            #     print('chats----', chat.to_dict())
-            return jsonify({"message": 'success'}), 200
+        if chat_collection:
+            collectionData = db.collection(str(uid)).document(
+                type).collection(chat_id).stream()
+            all_messages = [message.to_dict() for message in collectionData]
+            return jsonify({'response':all_messages}), 200
+
+            # return jsonify({"message": 'tyes'}), 200
     except Exception as e:
         return f'{e}'
-
 
 
 @app.route('/v1/chat/completions', methods=['POST'])
@@ -103,45 +106,50 @@ def ask():
         userRequest = request.json['data']
         collection_type = request.json['collection_type']
 
-        uid =  request.user['uid']
+        uid = request.user['uid']
         userData = userRequest['messages'][0]['content']
 
         jsonResponse = json.dumps(userRequest)
-        response = requests.post(f'{open_ai_url}/v1/chat/completions', jsonResponse, headers=OpenAIHeaders)
+        response = requests.post(
+            f'{open_ai_url}/v1/chat/completions', jsonResponse, headers=OpenAIHeaders)
         content = response.json()
-        
+
         chatGPTResponse = content['choices'][0]['message']['content']
-        message = create(uid=uid,user=userData,chatgpt=chatGPTResponse, collection_type=collection_type)
+
+        createdResponse = create(
+            uid=uid, user=userData, chatgpt=chatGPTResponse, collection_type=collection_type)
 
         # message = create(uid=uid, user='what is Stack', chatgpt='Stack is related to data structures', collection_type=collection_type)
-        return jsonify({"message": message}), 200
+        return jsonify({"response": createdResponse}), 200
     except Exception as e:
         return f'Error Occured: {e}', 400
 
-def create(uid,user,chatgpt,collection_type):
+
+def create(uid, user, chatgpt, collection_type):
     try:
         message = {}
-        titleData={}
-  
+        titleData = {}
+
         title_collection_length = 0
         chat_collection_length = 0
 
         if db.collection(str(uid)):
-            chat_collection = db.collection(str(uid)).document('messages').collection(collection_type).count().get()
+            chat_collection = db.collection(str(uid)).document(
+                'messages').collection(collection_type).count().get()
             chat_collection_length = chat_collection[0][0].value
-          
-                
+
         message['id'] = chat_collection_length
         message['user'] = user
         message['chatgpt'] = chatgpt
 
         titleData['title'] = user[:25]
 
-
-        db.collection(str(uid)).document('messages').collection(collection_type).document(str(chat_collection_length)).set(message)
+        db.collection(str(uid)).document('messages').collection(
+            collection_type).document(str(chat_collection_length)).set(message)
 
         if not db.collection(str(uid)).document('title').collection(collection_type).document(str(title_collection_length)).get().exists:
-            db.collection(str(uid)).document('title').collection(collection_type).document(str(title_collection_length)).set(titleData)
+            db.collection(str(uid)).document('title').collection(
+                collection_type).document(str(title_collection_length)).set(titleData)
 
         return message
     except Exception as e:
